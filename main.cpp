9b6a429cf0e09ab04f4db7c727577615e311e4ab
gobbly <sinmorek@protonmail.com>
2018-09-15 19:02:00 +0200

    options

--- main.cpp
+++ main.cpp
@@ -11,16 +11,18 @@ complete from scratch
 
 using namespace std;
 
-
 class Date
 {
 public:
-    int month, year, startDay;
+    int day, month, year, startDay;
     int daysInMonth[12];
     void get_date();
     bool isLeapYear(int);
     void set_daysInMonth();
     void print_calender(int);
+    void add_plans(int, int, int);
+    void change_plans(int, int, int);
+    void delete_plans(int, int, int);
 };
 
 // User inputs a date (mm.yyyy)
@@ -71,21 +73,24 @@ void Date::set_daysInMonth()
 
 void Date::print_calender(int month)
 {
-    vector<string> weekday = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-    cout << weekday[0] << "\t" << weekday[1] << "\t" << weekday[2] << "\t" << weekday[3] << "\t";
-    cout << weekday[4] << "\t" << weekday[5] << "\t" << weekday[6] << "\n";
     int findDay = 0;
     int startDay = 0;
+    vector<string> weekday = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+    cout << weekday[0] << "\t" << weekday[1] << "\t" << weekday[2] << "\t" << weekday[3] << "\t";
+    cout << weekday[4] << "\t" << weekday[5] << "\t" << weekday[6] << "\n";
+
     for (int i = 1; i <= month; i++)
     {
         findDay += daysInMonth[i];
     }
+
     if (month == 1)
         startDay = 0;
     else
         startDay = (findDay-daysInMonth[month]) % 7;
    
-   
+    // So that the days are printed on the correct place in calender the first week 
     switch (month) {
         case 2:  cout << setw(startDay*8+1); break; 
         case 3:  cout << setw(startDay*8+1); break; 
@@ -108,6 +113,20 @@ void Date::print_calender(int month)
             cout << "\n";
 
     }
+    cout << "\n\n\n";
+}
+
+void Date::add_plans(int day, int month, int year)
+{
+    cout << "\nYou want to add plans for " << day << "." << month << "." << year << "\n";
+}
+void Date::change_plans(int day, int month, int year)
+{
+    cout << "\nYou want to change plans for " << day << "." << month << "." << year << "\n";
+}
+void Date::delete_plans(int day, int month, int year)
+{
+    cout << "\nYou want to delete plans for " << day << "." << month << "." << year << "\n";
 }
 
 int main() 
@@ -118,7 +137,25 @@ int main()
     if (date.isLeapYear(date.year))
         date.daysInMonth[2] = 29;
     date.print_calender(date.month);
-    cout << "\nDays in month: " << date.daysInMonth[date.month];
+    cout << "Enter a day of the month: ";
+    cin >> date.day;
+    cout << "\nWhat do you want to do?\n";
+    cout << "1. Add plans\n";
+    cout << "2. Change plans\n";
+    cout << "3. Delete plans\n";
+    cout << "4. Print calender again\n";
+    cout << "5. Enter new month\n";
+    cout << "6. Enter new day\n";
+    cout << "Enter option: ";
+    int options;
+    cin >> options;
+    switch(options) {
+        case 1: date.add_plans(date.day, date.month, date.year); break; 
+        case 2: date.change_plans(date.day, date.month, date.year); break; 
+        case 3: date.delete_plans(date.day, date.month, date.year); break; 
+        case 4: date.print_calender(date.month); break; 
+    }
+    
     return 0;
 }
     
